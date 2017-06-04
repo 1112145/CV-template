2d320e52fb0f828d1fbecd13ac68fc7fbda58dae
Nguyen Dang Khoa <dangkhoaicao@gmail.com>
2017-06-04 15:04:54 +0700

    change assets path

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
+
 
 app.use(express.static(__dirname));
 
 app.get('/',function(res,req){
-    req.sendfile('index.html');
+    req.sendfile(path.join(__dirname,'index.html'));
 })
 
 app.listen(process.env.PORT || 8001, function(){
