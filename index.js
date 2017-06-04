3e107cfecd1e8bed7ac23ee6eb767e28b32e02c3
Nguyen Dang Khoa <dangkhoaicao@gmail.com>
2017-06-04 15:10:55 +0700

    fix

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var path = require('path');
 
 var app = express();
 
+console.log(path.join(__dirname,'assets/index.html'));
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'assets')));
 
 app.get('/',function(res,req){
-    req.sendfile(path.join(__dirname,'index.html'));
+    req.sendfile(path.join(__dirname,'assets/index.html'));
 })
 
 app.listen(process.env.PORT || 8001, function(){
